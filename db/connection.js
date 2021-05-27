ce99af793ebd2d7558415847c16930112ab97ca1
Teller Wetzel <tellerwetzel@yahoo.com>
2021-05-27 11:00:54 -0600

    got schema tables done

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-require('dotenv').config({ path: './.env' });
+cd
 
 // Connect to database
 const db = mysql.createConnection(
---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT  PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(6,2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES department(id) ON DELETE SET NULL
+);
